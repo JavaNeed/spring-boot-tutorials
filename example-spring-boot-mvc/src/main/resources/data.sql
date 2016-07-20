64206709c76c342e9dd9ee62ad4e244b5c2c009f
prateek <prateek.ashtikar512@yahoo.in>
2016-07-20 21:38:57 +0530

    Spring Boot MVC

---
+++ example-spring-boot-mvc/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (id, password) VALUES ('John', 'pass1');
+INSERT INTO user (id, password) VALUES ('Chris', 'pass1');
+INSERT INTO user (id, password) VALUES ('Raj', 'pass1');
+INSERT INTO user (id, password) VALUES ('Deepak', 'pass1');
